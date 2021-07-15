b95a114aca0bd7ca6459dc5f639a463d0d765b36
andreybannikov <andrewnnov@yandex.ru>
2021-07-15 08:42:55 +0300

    deleted path to drivers for homework

--- conftest.py
+++ conftest.py
@@ -21,13 +21,12 @@ def browser(request):
         print("\nstart chrome browser for test..")
         options = Options()
         options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
-        browser = webdriver.Chrome("C:\\Projects\\stepik_qa\\driver\\chromedriver.exe", options=options)
+        browser = webdriver.Chrome(options=options)
     elif browser_name == "firefox":
         print("\nstart firefox browser for test..")
         fp = webdriver.FirefoxProfile()
         fp.set_preference("intl.accept_languages", user_language)
-        browser = webdriver.Firefox(executable_path="C:\\Projects\\stepik_qa\\driver\\gecko\\geckodriver.exe",
-                                    firefox_profile=fp)
+        browser = webdriver.Firefox(firefox_profile=fp)
     else:
         raise pytest.UsageError("--browser_name should be chrome or firefox")
     yield browser
